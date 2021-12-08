6673fc8e919677a8141ad36de6783e8276ee7f74
asianlad <husanisomiddinov2006@gmail.com>
2021-12-08 18:19:51 +0500

    'birinchi'

---
+++ app.js
@@ -0,0 +1,36 @@
+/*function greet(name){
+ let qw = 'Hello, how are you doing today?' ;
+let greet = 'Ryan';
+let split = qw.split('');
+let abc = `${split[0] + greet + split[1]}`;
+console.log(abc)
+} */
+
+/*
+for(let i = 10 ; i<50 ; i++){
+if (i%3 === 0 ){
+    console.log(i)
+}
+} */
+
+
+
+
+/*function hello (name) {
+    if(typeof name === String){
+        console.log('Sorry something went wrong , please check your entry form again');
+    }
+    else{
+        let hello = name.split(' ');
+        console.log(`Hello ${hello[1]} how are you doing i know that your name is ${hello[0]}`);
+    }
+   
+}
+
+hello('Husan Isomiddinov');  */
+
+
+
+  
+
+
